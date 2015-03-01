0e3b3729accd77a4638f67be4602b1fce544d7df
Paul Annesley <paul@annesley.cc>
2015-02-28 17:33:03 -0800

    main.c: send 3ms reset pulse to ADB bus.

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+#define ADB_DATA_MASK (0b00000001)
+#define ADB_HIGH() (PORTB |= ADB_DATA_MASK)
+#define ADB_LOW() (PORTB &= ~ADB_DATA_MASK)
+
 void setup();
 void loop();
+
+void adb_reset();
+void adb_data_mode_input();
+void adb_data_mode_output();
 
 int main() {
   setup();
@@ -10,12 +18,25 @@ int main() {
 }
 
 void setup() {
-  DDRB = 0xFF;
+  adb_reset();
 }
 
 void loop() {
-  PORTB = 0xFF;
-  _delay_ms(500);
-  PORTB = 0x00;
-  _delay_ms(500);
+}
+
+void adb_reset() {
+  adb_data_mode_output();
+  ADB_LOW();
+  _delay_ms(3);
+  adb_data_mode_input();
+}
+
+void adb_data_mode_input() {
+  DDRB &= ~ADB_DATA_MASK;
+  PORTB &= ~ADB_DATA_MASK; // hi-Z input
+}
+
+void adb_data_mode_output() {
+  ADB_HIGH(); // output high to match pull-up resistor
+  DDRB |= ADB_DATA_MASK;
 }
