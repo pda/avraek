5f3cfa718ea5d0d1038109ef7ad9be5b099d4cdf
Paul Annesley <paul@annesley.cc>
2015-03-01 15:04:39 -0800

    avr-uart integrated; status output.

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 MCU = atmega328p
-F_CPU = 16000000
+F_CPU = 16000000L
+UART_BAUD = 9600
 PROGRAMMER = arduino
 PORT = /dev/cu.usbmodem14231
 
@@ -8,7 +9,7 @@ ADB_DDR = DDRB
 ADB_DATA_PIN = 0
 
 ADB_DEFINE_FLAGS = -DADB_PORT=$(ADB_PORT) -DADB_DDR=$(ADB_DDR) -DADB_DATA_PIN=$(ADB_DATA_PIN)
-CC_DEFINE_FLAGS = -DF_CPU=$(F_CPU) $(ADB_DEFINE_FLAGS)
+CC_DEFINE_FLAGS = -DF_CPU=$(F_CPU) -DUART_BAUD=$(UART_BAUD) $(ADB_DEFINE_FLAGS)
 CC_FLAGS = -std=c99 -Wall -g -Os -mmcu=$(MCU) $(CC_DEFINE_FLAGS)
 
 .PHONY: all
@@ -26,4 +27,4 @@ main.hex: main.elf
 	avr-objcopy -j .text -j .data -O ihex main.elf main.hex
 
 main.elf: main.c adb.c
-	avr-gcc $(CC_FLAGS) -Wl,-Map,main.map -o main.elf main.c adb.c
+	avr-gcc $(CC_FLAGS) -Wl,-Map,main.map -o main.elf main.c adb.c avr-uart/uart.c
--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include "adb.h"
+#include "avr-uart/uart.h"
+
+#include <avr/interrupt.h>
 #include <avr/io.h>
 #include <util/delay.h>
 
 void setup();
 void loop();
+void initialize_uart();
+void initialize_adb();
+void initialize_adb_keyboard();
 
 int main() {
   setup();
@@ -11,15 +17,32 @@ int main() {
 }
 
 void setup() {
+  initialize_uart();
+  uart0_puts("UART initialized\r\n");
+  initialize_adb();
+  uart0_puts("ADB initialized\r\n");
+  initialize_adb_keyboard();
+  uart0_puts("Keyboard initialized\r\n");
+}
+
+void loop() {
+  _delay_ms(200);
+}
+
+void initialize_uart() {
+  sei();
+  uart0_init(UART_BAUD_SELECT(UART_BAUD, F_CPU));
+}
+
+void initialize_adb() {
   adb_reset();
-  _delay_ms(1); // arbitrary
+}
+
+void initialize_adb_keyboard() {
   struct adb_cmd cmd = {
     .address = ADB_KB_ADDRESS,
     .command = ADB_COMMAND_TALK,
     .reg = ADB_REGISTER_INFO
   };
   adb_send_command(cmd);
-}
-
-void loop() {
 }
