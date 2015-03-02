7935fd1bf0db616521e9529ca348c701c5530bed
Paul Annesley <paul@annesley.cc>
2015-03-01 21:58:17 -0800

    Compile flags for ADB_PORT, ADB_PORT_PIN overhauled.

--- adb.h
+++ adb.h
@@ -2,13 +2,39 @@
 
 #include <stdint.h>
 
-// Makefile provides these as compiler flags:
-// #define ADB_PORT PORTB
-// #define ADB_PIN PINB
-// #define ADB_DDR DDRB
-// #define ADB_DATA_PIN
+#ifdef ADB_PORTA
+  #define ADB_PORT PORTA
+  #define ADB_PIN PINA
+  #define ADB_DDR DDRA
+#elif ADB_PORTB
+  #define ADB_PORT PORTB
+  #define ADB_PIN PINB
+  #define ADB_DDR DDRB
+#elif ADB_PORTC
+  #define ADB_PORT PORTC
+  #define ADB_PIN PINC
+  #define ADB_DDR DDRC
+#elif ADB_PORTD
+  #define ADB_PORT PORTD
+  #define ADB_PIN PIND
+  #define ADB_DDR DDRD
+#elif ADB_PORTE
+  #define ADB_PORT PORTE
+  #define ADB_PIN PINE
+  #define ADB_DDR DDRE
+#elif ADB_PORTF
+  #define ADB_PORT PORTF
+  #define ADB_PIN PINF
+  #define ADB_DDR DDRF
+#else
+  #error "ADB_PORT{A,B,C,D,E,F} required"
+#endif
 
-#define ADB_DATA_MASK (1 << ADB_DATA_PIN)
+#ifndef ADB_PORT_PIN
+  #error "ADB_PORT_PIN required"
+#endif
+
+#define ADB_DATA_MASK (1 << ADB_PORT_PIN)
 
 #define ADB_HIGH() (ADB_PORT |= ADB_DATA_MASK)
 #define ADB_LOW() (ADB_PORT &= ~ADB_DATA_MASK)
