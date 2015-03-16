74e663a4bac3b74374840f15b9c30fcf878ca167
Paul Annesley <paul@annesley.cc>
2015-03-15 17:21:33 -0700

    aek2_keys.h: ADB keycode constants; manually collated.

---
+++ aek2_keys.h
@@ -0,0 +1,109 @@
+/**
+ * Apple Extended Keyboard II keycodes.
+ * Recorded from left to right, top to bottom.
+ * Recorded without activating extended mode.
+ */
+#define ADB_KEY_ESC        0x35
+#define ADB_KEY_F1         0x7A
+#define ADB_KEY_F2         0x78
+#define ADB_KEY_F3         0x63
+#define ADB_KEY_F4         0x76
+#define ADB_KEY_F5         0x60
+#define ADB_KEY_F6         0x61
+#define ADB_KEY_F7         0x62
+#define ADB_KEY_F8         0x64
+#define ADB_KEY_F9         0x65
+#define ADB_KEY_F10        0x6D
+#define ADB_KEY_F11        0x67
+#define ADB_KEY_F12        0x6F
+#define ADB_KEY_F13        0x69
+#define ADB_KEY_F14        0x6B
+#define ADB_KEY_F15        0x71
+#define ADB_KEY_GRAVE      0x32
+#define ADB_KEY_1          0x12
+#define ADB_KEY_2          0x13
+#define ADB_KEY_3          0x14
+#define ADB_KEY_4          0x15
+#define ADB_KEY_5          0x17
+#define ADB_KEY_6          0x16
+#define ADB_KEY_7          0x1A
+#define ADB_KEY_8          0x1C
+#define ADB_KEY_9          0x19
+#define ADB_KEY_0          0x1D
+#define ADB_KEY_DASH       0x1B
+#define ADB_KEY_EQUAL      0x18
+#define ADB_KEY_BKSP       0x33
+#define ADB_KEY_INS        0x72
+#define ADB_KEY_HOME       0x73
+#define ADB_KEY_PGUP       0x74
+#define ADB_KEY_NUM        0x47
+#define ADB_KEY_KPEQUAL    0x51
+#define ADB_KEY_KPSLASH    0x4B
+#define ADB_KEY_KPSTAR     0x43
+#define ADB_KEY_TAB        0x30
+#define ADB_KEY_Q          0x0C
+#define ADB_KEY_W          0x0D
+#define ADB_KEY_E          0x0E
+#define ADB_KEY_R          0x0F
+#define ADB_KEY_T          0x11
+#define ADB_KEY_Y          0x10
+#define ADB_KEY_U          0x20
+#define ADB_KEY_I          0x22
+#define ADB_KEY_O          0x1F
+#define ADB_KEY_P          0x23
+#define ADB_KEY_LBRACKET   0x21
+#define ADB_KEY_RBRACKET   0x1E
+#define ADB_KEY_BSLASH     0x2A
+#define ADB_KEY_DEL        0x75
+#define ADB_KEY_END        0x77
+#define ADB_KEY_PGDN       0x79
+#define ADB_KEY_KP7        0x59
+#define ADB_KEY_KP8        0x5B
+#define ADB_KEY_KP9        0x5C
+#define ADB_KEY_KPMINUS    0x4E
+#define ADB_KEY_CAPS       0x39
+#define ADB_KEY_A          0x00
+#define ADB_KEY_S          0x01
+#define ADB_KEY_D          0x02
+#define ADB_KEY_F          0x03
+#define ADB_KEY_G          0x05
+#define ADB_KEY_H          0x04
+#define ADB_KEY_J          0x26
+#define ADB_KEY_K          0x28
+#define ADB_KEY_L          0x25
+#define ADB_KEY_SEMICOL    0x29
+#define ADB_KEY_SQUOT      0x27
+#define ADB_KEY_RETURN     0x24
+#define ADB_KEY_KP4        0x56
+#define ADB_KEY_KP5        0x57
+#define ADB_KEY_KP6        0x58
+#define ADB_KEY_KPPLUS     0x45
+#define ADB_KEY_SHIFT      0x38 /* left shift */
+#define ADB_KEY_Z          0x06
+#define ADB_KEY_X          0x07
+#define ADB_KEY_C          0x08
+#define ADB_KEY_V          0x09
+#define ADB_KEY_B          0x0B
+#define ADB_KEY_N          0x2D
+#define ADB_KEY_M          0x2E
+#define ADB_KEY_COMMA      0x2B
+#define ADB_KEY_PERIOD     0x2F
+#define ADB_KEY_FSLASH     0x2C
+/*#define ADB_KEY_SHIFT    0x38    right shift */
+#define ADB_KEY_UP         0x3E
+#define ADB_KEY_KP1        0x53
+#define ADB_KEY_KP2        0x54
+#define ADB_KEY_KP3        0x55
+#define ADB_KEY_KPENTER    0x4C
+#define ADB_KEY_CTRL       0x36 /* left control */
+#define ADB_KEY_OPTION     0x3A /* left option */
+#define ADB_KEY_CMD        0x37 /* left command */
+#define ADB_KEY_SPACE      0x31
+/*#define ADB_KEY_CMD      0x37    right command */
+/*#define ADB_KEY_OPTION   0x3A    right option */
+/*#define ADB_KEY_CTRL     0x36    right control */
+#define ADB_KEY_LEFT       0x3B
+#define ADB_KEY_DOWN       0x3D
+#define ADB_KEY_RIGHT      0x3C
+#define ADB_KEY_KP0        0x52
+#define ADB_KEY_KPPERIOD   0x41
