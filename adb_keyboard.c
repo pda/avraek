35ff61ff715754f9fd26a53823180cb4f03c6309
Paul Annesley <paul@annesley.cc>
2015-03-16 21:24:35 -0700

    adb_keyboard tidied; use constants, remove unused include.

--- adb_keyboard.c
+++ adb_keyboard.c
@@ -26,7 +26,7 @@ void adb_keyboard_poll(struct adb_response_16 * response) {
   struct adb_cmd cmd = {
     .address = ADB_KB_ADDRESS,
     .command = ADB_COMMAND_TALK,
-    .reg = 0
+    .reg = ADB_REGISTER_PRIMARY
   };
   adb_send_command(&cmd);
   adb_receive_16(response);
@@ -38,7 +38,7 @@ static void adb_keyboard_animate_leds() {
   struct adb_cmd listen = {
     .address = ADB_KB_ADDRESS,
     .command = ADB_COMMAND_LISTEN,
-    .reg = 2
+    .reg = ADB_REGISTER_2
   };
   uint16_t states[] = {
     //SCN: Scroll, Caps, Num
--- adb_keyboard.h
+++ adb_keyboard.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <stdint.h>
-
 // Functions
 
 void adb_keyboard_initialize();
